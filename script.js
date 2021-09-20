2e3b2d86cbba2f63f875b74aea004953ebe3c955
sannetyes <59413301+sannetyes@users.noreply.github.com>
2021-09-20 12:26:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById("canvas")
-var ctx = canvas.getContext("2d")
+let canvas = document.getElementById("canvas")
+let ctx = canvas.getContext("2d")
 
 let snake = [ {x:40, y:200},
               {x:30, y:200},
@@ -7,33 +7,60 @@ let snake = [ {x:40, y:200},
               {x:10, y:200},
               {x:0, y:200}
 ]
-var i = 0
 function drawSnakePixel(pixel){
     ctx.fillStyle = "green"
     ctx.strokeStyle = "black"
     ctx.fillRect(pixel.x, pixel.y, 10, 10)
     ctx.strokeRect(pixel.x,pixel.y,10,10)
 }
-function clear (){/* 
-    ctx.fillStyle = "white"
-    ctx.strokeStyle = "white"
-    ctx.fillRect(snake[4].x,200,10,10)
-    ctx.strokeRect(snake[4].x,200,10,10) */
-    ctx.clearRect(snake[4].x,200,10,10)
-}
 function drawSnake(){
     snake.forEach(drawSnakePixel)
 }
+let up = false
+let down = false
+let right = false
+let left = false
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+      case 'ArrowUp':
+        up = true
+        break
+      case 'ArrowDown':
+        down = true
+        break
+      case 'ArrowLeft':
+        left = true
+        break
+      case 'ArrowRight':
+        right = true
+        break
+    }
+  })
 function actionSnake(){
     setTimeout(() => {
-            snake.unshift({x:(snake[0].x + 10), y:200})
-            console.log(snake)
-            drawSnake()
-            snake.forEach(clear)
-            if( i <(canvas.clientWidth) / 10 ){
-                snake.pop()
-                actionSnake()
-            }
-    },2000)
+        if (up){
+            snake.unshift({x:snake[0].x ,y: snake[0].y - 10})
+        }
+        else if (down){
+            snake.unshift({x:snake[0].x , y:snake[0].y + 10})
+        }
+        else if(right){
+            snake.unshift({x:snake[0].x + 10, y:snake[0].y})
+        }
+        else if (left){
+            snake.unshift({x:(snake[0].x - 10), y:snake[0].y})
+        }
+        console.log(snake)
+        ctx.fillStyle = "#fff"
+        ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)
+        drawSnake()
+        if( snake[0].x < 400 && snake[0].y < 400){
+            snake.pop()
+            actionSnake()
+        }
+    },500)
 }
-actionSnake()
+function main() {
+    actionSnake()
+}
+main()
