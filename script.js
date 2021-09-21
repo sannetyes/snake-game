23da7bc89ec9d17f13ce4cc4ab8a0ae4ac956900
SameerCrestha <ddav.crestha@gmail.com>
2021-09-21 11:19:06 +0545

    fixed movement bug and shrinking at start bug

--- script.js
+++ script.js
@@ -1,68 +1,70 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+let canvas = document.getElementById("canvas")
+let ctx = canvas.getContext("2d")
 
-let snake = [
-  { x: 40, y: 200 },
-  { x: 30, y: 200 },
-  { x: 20, y: 200 },
-  { x: 10, y: 200 },
-  { x: 0, y: 200 },
-];
-function drawSnakePixel(pixel) {
-  ctx.fillStyle = "green";
-  ctx.strokeStyle = "black";
-  ctx.fillRect(pixel.x, pixel.y, 10, 10);
-  ctx.strokeRect(pixel.x, pixel.y, 10, 10);
+let snake = [ {x:40, y:200},
+              {x:30, y:200},
+              {x:20, y:200},
+              {x:10, y:200},
+              {x:0, y:200}
+]
+function drawSnakePixel(pixel){
+    ctx.fillStyle = "green"
+    ctx.strokeStyle = "black"
+    ctx.fillRect(pixel.x, pixel.y, 10, 10)
+    ctx.strokeRect(pixel.x,pixel.y,10,10)
 }
-function drawSnake() {
-  snake.forEach(drawSnakePixel);
+function drawSnake(){
+    snake.forEach(drawSnakePixel)
 }
-let up = false;
-let down = false;
-let right = true;
-let left = false;
-window.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "ArrowUp":
-      up = true;
-      down = right = left = false;
-      break;
-    case "ArrowDown":
-      down = true;
-      up = right = left = false;
-      break;
-    case "ArrowLeft":
-      left = true;
-      up = down = right = false;
-      break;
-    case "ArrowRight":
-      right = true;
-      up = down = left = false;
-      break;
-  }
-});
-function actionSnake() {
-  setTimeout(() => {
-    if (up) {
-      snake.unshift({ x: snake[0].x, y: snake[0].y - 10 });
-    } else if (down) {
-      snake.unshift({ x: snake[0].x, y: snake[0].y + 10 });
-    } else if (right) {
-      snake.unshift({ x: snake[0].x + 10, y: snake[0].y });
-    } else if (left) {
-      snake.unshift({ x: snake[0].x - 10, y: snake[0].y });
+let up = false
+let down = false
+let right = true
+let left = false
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+      case 'ArrowUp':
+        up = true
+        down=right=left=false
+        break
+      case 'ArrowDown':
+        down = true
+        up=right=left=false
+        break
+      case 'ArrowLeft':
+        left = true
+        up=down=right=false
+        break
+      case 'ArrowRight':
+        right = true
+        up=down=left=false
+        break
     }
-    console.log(snake);
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    drawSnake();
-    if (snake[0].x < 400 && snake[0].y < 400) {
-      snake.pop();
-      actionSnake();
-    }
-  }, 500);
+  })
+function actionSnake(){
+    setTimeout(() => {
+        if (up){
+            snake.unshift({x:snake[0].x ,y: snake[0].y - 10})
+        }
+        else if (down){
+            snake.unshift({x:snake[0].x , y:snake[0].y + 10})
+        }
+        else if(right){
+            snake.unshift({x:snake[0].x + 10, y:snake[0].y})
+        }
+        else if (left){
+            snake.unshift({x:(snake[0].x - 10), y:snake[0].y})
+        }
+        console.log(snake)
+        ctx.fillStyle = "#fff"
+        ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)
+        drawSnake()
+        if( snake[0].x < 400 && snake[0].y < 400){
+            snake.pop()
+            actionSnake()
+        }
+    },500)
 }
 function main() {
-  actionSnake();
+    actionSnake()
 }
-main();
+main()
